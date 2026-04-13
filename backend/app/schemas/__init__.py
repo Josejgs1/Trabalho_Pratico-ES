bd0c4958ea4d977e66282b40f211c8b03ab82127
Clarice Oliveira <wclarice14@gmail.com>
2026-04-12 22:41:18 -0300

    Add record schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,8 +1,12 @@
+from app.schemas.record import RecordCreate, RecordRead, RecordUpdate
 from app.schemas.user import AuthTokenRead, UserCreate, UserLogin, UserRead
 from app.schemas.venue import VenueCreate, VenueRead, VenueUpdate
 
 __all__ = [
     "AuthTokenRead",
+    "RecordCreate",
+    "RecordRead",
+    "RecordUpdate",
     "UserCreate",
     "UserLogin",
     "UserRead",
