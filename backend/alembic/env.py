7632f009b9dd2999281d45308c4aed79f39b4eb7
Kauan Pereira <kauanteixeirapereira5@gmail.com>
2026-05-04 15:25:57 -0300

    fix: alembic reads DATABASE_URL from environment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
@@ -11,6 +12,9 @@ config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+if os.getenv("DATABASE_URL"):
+    config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 target_metadata = Base.metadata
 
